381c231e69f471a90264c3f3cebdb1f180fa6a93
Louis Dyrhauge <94385943+louvalman@users.noreply.github.com>
2026-01-11 22:22:24 +0100

    package.json updated along with configs for favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 // webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +13,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets/favicon', to: 'assets/favicon' }],
     }),
   ],
   module: {
